09f903a649aed3644df02ad7bbfd994e0394f0f8
MiraKaffe <logjam442@icloud.com>
2024-09-27 13:54:09 -0700

    Update GameEnv.js

--- assets/js/rpg/GameEnv.js
+++ assets/js/rpg/GameEnv.js
@@ -49,8 +49,8 @@ class GameEnv {
         this.setCanvas();
         this.setTop();
         this.setBottom();
-        this.innerWidth = 1200
-        this.innerHeight = 500
+        this.innerWidth = 800
+        this.innerHeight = 450
         this.size();
     }
 
